24f7d3bec62e40d54ec6d20d27dd527a3f06a0eb
Diane Sparks <duncan.sprks@gmail.com>
2021-01-30 13:13:38 -0800

    Add startup sequence

--- scripts/main_scene.gd
+++ scripts/main_scene.gd
@@ -1,7 +1,29 @@
 extends Spatial
 
 export(bool) var startup_sequence := false
+export(Resource) var file_to_display: Resource
+
+var boot_text := [
+	"\n\n\tChecking boot disk...",
+	"\n\n\n\tReading configuration data...",
+	"\n\n\tInitializing filegraph table...",
+	"\n\n\n\tPerforming final setup...",
+]
 
 func _ready() -> void:
 	if startup_sequence:
 		($AnimationPlayer as AnimationPlayer).play("Startup")
+
+
+func add_boot_text(index: int) -> void:
+	($Viewport/Screen/BootText as RichTextLabel).text += boot_text[index]
+
+
+func clear_screen() -> void:
+	($Viewport/Screen as Screen).set_screen_text("")
+	$Viewport/Screen/BootText.hide()
+	$Viewport/Screen/Logo.hide()
+	
+	
+func show_start_text() -> void:
+	($Viewport/Screen as Screen).set_screen_text(file_to_display.text)
--- scripts/room_props/monitor.gd
+++ scripts/room_props/monitor.gd
@@ -1,7 +1,5 @@
 extends Spatial
 
-export(Resource) var file_to_display: Resource
-
 onready var screen_viewport := get_node("../Viewport") as Viewport
 onready var screen := get_node("../Viewport/Screen") as Screen
 
@@ -10,5 +8,3 @@ func _ready() -> void:
 	var tex := screen_viewport.get_texture()
 	tex.set_flags(ViewportTexture.FLAG_FILTER)
 	get_node("Cube").get_surface_material(3).set_shader_param("viewport_texture", tex)
-	
-	screen.call_deferred("set_screen_text", file_to_display.text)
